598482ff02ab772ae342d087b2d163db02df8d1c
Ali Haider Nadeem <alihdrndm@gmail.com>
2020-10-31 09:37:42 -0600

    the never type

--- app.ts
+++ app.ts
@@ -7,3 +7,14 @@ userInput = "Ali";
 if (typeof userInput === "string") {
   userName = userInput;
 }
+
+function handleTheError(message: string, code: number) {
+  throw {
+    message,
+    code
+  };
+  //the script breaks and doesn't go any further from here
+  console.log("Did the script break");
+}
+
+handleTheError("We have an error", 500);
