124cbdd01e6e220de21650d528b507c2f1de2df7
Ali Haider Nadeem <alihdrndm@gmail.com>
2020-11-02 15:31:42 -0600

    callback types

--- app.ts
+++ app.ts
@@ -16,3 +16,15 @@ combineValues = add;
 combineValues = printResult; // notice this assignment throws error
 
 console.log(combineValues(8, 8));
+
+function addAndHandle(n1: number, n2: number, cb: (a: number) => void) { 
+    //note that the void here means that in addAndHandle function, we're not going
+    //to return anything from the 'cb' callback, because anything we might return
+    //will not be used
+    let result = n1 + n2;
+    cb(result)
+}
+
+addAndHandle(3, 5, (res) => {
+    console.log('res', res);
+})
