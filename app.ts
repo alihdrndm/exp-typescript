52eb96948d6e2f7a5ac4983e4c91378c1da44d0c
Ali Haider Nadeem <alihdrndm@gmail.com>
2020-11-03 15:31:42 -0600

    giving function types to variables

--- app.ts
+++ app.ts
@@ -1,11 +1,18 @@
+// our function with 2 parameters
 function add(n1: number, n2: number): number {
     return n1 + n2; 
 }
 
-// console.log(add(11, 31));
-
+// our function with only 1 parameter
 function printResult(num: number)  {
     console.log('Resulted', + num);
 }
 
-console.log(printResult(add(11, 32)));
+// declaring a variable and giving it a type of Function
+let combineValues: (a: number, b: number) => number;
+
+combineValues = add;
+
+combineValues = printResult; // notice this assignment throws error
+
+console.log(combineValues(8, 8));
