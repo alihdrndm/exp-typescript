760cf7cf0a390c935f2adbe6e8c710d0f52c0e1d
Ali Haider Nadeem <alihdrndm@gmail.com>
2020-11-03 11:50:35 -0600

    function return type

--- app.ts
+++ app.ts
@@ -1,16 +1,5 @@
-type Combinable = number | string;
-type ConversionDescriptor = 'to-number' | 'to-text'
-
-function combine(input1: Combinable, input2: Combinable, convertResult: ConversionDescriptor) {
-    let result;
-   if (typeof input1 === 'number' && typeof input2 === 'number' || convertResult === 'to-number') {
-       result = +input1 + +input2;
-   }else {
-       result = input1.toString() + input2.toString();
-   } 
-    return result; 
+function add(n1: number, n2: number): number {
+    return n1 + n2; 
 }
 
-console.log(combine(40, 34, 'to-number'));
-
-console.log(combine('Ali', 'Haider', 'to-text'));
+console.log(add(11, 31));
