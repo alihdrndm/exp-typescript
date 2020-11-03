57f0b6a351724146bb1c1baaa1c3da7df45ef935
Ali Haider Nadeem <alihdrndm@gmail.com>
2020-11-03 13:50:35 -0600

    difference between void and undefined

--- app.ts
+++ app.ts
@@ -2,4 +2,10 @@ function add(n1: number, n2: number): number {
     return n1 + n2; 
 }
 
-console.log(add(11, 31));
+// console.log(add(11, 31));
+
+function printResult(num: number)  {
+    console.log('Resulted', + num);
+}
+
+console.log(printResult(add(11, 32)));
