572aa6fc9aa5ea539f94acf417c7aeec07282b4b
Ali Haider Nadeem <alihdrndm@gmail.com>
2022-05-31 13:02:46 +0500

    tuples

--- app.ts
+++ app.ts
@@ -1,14 +1,17 @@
-// const person: {
-//     name: string;
-//     age: number;
-// } = {
-    const person = {
+const person: {
+    name: string;
+    age: number;
+    hobbies: (string | number | string | boolean | {isImage: boolean; imageSize: string;})[];
+    role: [number, string]
+} = {
+    // const person = {
     name: 'Ali Haider Nadeem',
     age: 27,
-    hobbies: ['sports', 1, 'cooking', true, {isImage: true, imageSize: '500px'}]
+    hobbies: ['sports', 1, 'cooking', true, {isImage: true, imageSize: '500px'}],
+    role: [2, 'administrator']
 }
 
-// console.log(person.age);
+console.log(person.role);
 
 for (let hobby of person.hobbies) {
     // console.log(hobby);
